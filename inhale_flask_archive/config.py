48c9ddd876c47b1d4309404edc29a1a5585d3dd8
Matt Rigby <matt.rigby@bristol.ac.uk>
2025-04-22 16:40:58 +0000

    Edit project name

--- inhale_flask_archive/config.py
+++ inhale_flask_archive/config.py
@@ -3,7 +3,6 @@ from agage_archive.config import setup
 
 if __name__ == '__main__':
 
-    # Uncomment and set the project name:
-    #project_name = "PROJECTNAME"
+    project_name = "inhale_flask"
 
     setup(project_name)
